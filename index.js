d6e4f9bc243cc654e0ec11c95cbf91c29f87f4bc
Christine Kim <christi4@andrew.cmu.edu>
2021-05-09 01:28:21 -0400

    added more interactive buttons

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function show(id) {
   function hide(id) {
     document.getElementById(id).style.visibility = "hidden";
   }
+
+  function changeImage(id) {
+
+    document.getElementById(id).src = "assets/DM-2.png";
+}
