a44700fbc1e86013598250c5f219fb6b441acedb
Christine Kim <christi4@andrew.cmu.edu>
2021-05-18 16:46:01 -0400

    notifications

--- index.js
+++ index.js
@@ -6,7 +6,15 @@ function show(id) {
     document.getElementById(id).style.visibility = "hidden";
   }
 
-  function changeImage(id) {
+  function changeImageToDM(id) {
 
     document.getElementById(id).src = "assets/DM-2.png";
+}
+
+function showDisplay(id){
+  document.getElementById(id).style.display = "block";
+}
+
+function hideDisplay(id){
+  document.getElementById(id).style.display = "none";
 }
